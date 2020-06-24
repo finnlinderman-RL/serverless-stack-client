4822f0970bd78a53ec63191f2fa6954bb12344c7
finnlinderman-RL <finn.linderman@ridgelineapps.com>
2020-06-24 14:11:59 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    STRIPE_KEY: "pk_test_51GxMlYJMweC5ABL7fVurcHIKcshclWB1EZO7lYEwXK6uIcVjmWWVqfCUNW1uG3z75NlFRPIjL92mY0sXuQwLKqjI00Xwy3uDnf",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-2",
       BUCKET: "finn-notes-app-uploads"
@@ -13,4 +15,4 @@ export default {
       APP_CLIENT_ID: "6d2ib2nnsvhl5nt3ilcjm0niup",
       IDENTITY_POOL_ID: "us-east-2:3b20f0bc-6d42-481a-9dce-3e29575ef9a1"
     }
-  };
+};
