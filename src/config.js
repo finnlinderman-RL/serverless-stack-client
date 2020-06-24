011a1e65601afff9e7e4a29fbaaab4d7a2ecee21
finnlinderman-RL <finn.linderman@ridgelineapps.com>
2020-06-24 10:26:08 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "finn-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://4b9f5gylf2.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_JrMRMZK4f",
+      APP_CLIENT_ID: "6d2ib2nnsvhl5nt3ilcjm0niup",
+      IDENTITY_POOL_ID: "us-east-2:3b20f0bc-6d42-481a-9dce-3e29575ef9a1"
+    }
+  };
